1aa65072335a6750e57edeb2042753cbedb94c18
zarif007 <zarifhuq786@gmail.com>
2021-06-05 17:31:36 +0600

    Added mailing system on views

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,4 @@
+from django.contrib.sites.shortcuts import get_current_site
 from django.core.mail import send_mail
 from django.shortcuts import redirect, render
 
@@ -39,17 +40,25 @@ def task_creation(request):
         form = TaskModelForm(request.POST)
         
         if form.is_valid():
+            form.save()
+
             user_name = form.cleaned_data['member']
             user = User.objects.get(in_club_name=user_name)
 
-            send_mail(
-                subject='TaskManager: You have been assinged to a new task',
-                message=f'You have assinged to {form.cleaned_data["task_name"]}',
-                from_email='djtester321@gmail.com', 
-                recipient_list=[user.email, 'zarifhuq007@gmail.com']
-            )
-            print(user.email)
-            form.save()
+            domain = get_current_site(request).domain
+            task_name = form.cleaned_data['task_name']
+            task_id = Task.objects.get(task_name=task_name).id
+            task_url = f'http://{domain + "/task/" + str(task_id) }'
+
+            if(user.email != ''):
+                send_mail(
+                    subject='TaskManager: You have been assinged to a new task',
+                    message=f'You have assinged to {form.cleaned_data["task_name"]}, click {task_url} to view',
+                    from_email='djtester321@gmail.com', 
+                    recipient_list=[user.email, 'zarifhuq007@gmail.com']
+                )
+            
+            
             
             return redirect('task_listings')
           
@@ -68,13 +77,29 @@ def task_update(request, pk):
     users = User.objects.all()
 
     form = TaskModelForm(instance=task)
+
+    prev_task_member = task.member
     
     if request.method == 'POST':
         form = TaskModelForm(request.POST, instance=task)
 
         if form.is_valid():
-            print(form)
             form.save()
+
+            user_name = form.cleaned_data['member']
+            user = User.objects.get(in_club_name=user_name)
+
+            domain = get_current_site(request).domain
+            task_id = Task.objects.get(task_name=task.task_name).id
+            task_url = f'http://{domain + "/task/" + str(task_id) }'
+
+            if(user.email != '' or prev_task_member != user_name):
+                send_mail(
+                    subject='TaskManager: You have been assinged to a new task',
+                    message=f'You have assinged to {form.cleaned_data["task_name"]}, click {task_url} to view',
+                    from_email='djtester321@gmail.com', 
+                    recipient_list=[user.email, 'zarifhuq007@gmail.com']
+                )
                 
             return redirect('task_detail', task.id)
 
@@ -107,9 +132,23 @@ def task_apply(request, pk):
         form = TaskApplyModelForm(request.POST, instance=task)
         
         if form.is_valid():
-            print(form)
             form.save()
-                
+
+            user_name = form.cleaned_data['member']
+            user = User.objects.get(in_club_name=user_name)
+
+            domain = get_current_site(request).domain
+            task_id = Task.objects.get(task_name=task.task_name).id
+            task_url = f'http://{domain + "/task/" + str(task_id) }'
+            print(task_id)
+            if(user.email != ''):
+                send_mail(
+                    subject='TaskManager: You have been assinged to a new task',
+                    message=f'You have assinged to {task.task_name}, click {task_url} to view',
+                    from_email='djtester321@gmail.com', 
+                    recipient_list=[user.email, 'zarifhuq007@gmail.com']
+                )
+ 
             return redirect('task_detail', task.id)
 
     context = {
