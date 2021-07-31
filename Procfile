4c7e7f04b82f138a7177f659bb347c7e189c6220
zarif007 <zarifhuq786@gmail.com>
2021-08-01 01:55:11 +0600

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn tasker.wsgi --log-file -
--- tasker/settings.py
+++ tasker/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bucchrtm.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
